38b5c9307ef3948aa01223093b600f5cfc7ab142
unknown <luismoriano2@gmail.com>
2025-12-30 06:38:12 -0500

    Corrigiendo rutas de la API para Render

--- assets/js/app.js
+++ assets/js/app.js
@@ -152,7 +152,7 @@ document.addEventListener('DOMContentLoaded', () => {
         };
 
         try {
-            const res = await fetch(`${PYTHON_SERVER_URL}pedidos`, {
+            const res = await fetch(`${PYTHON_SERVER_URL}/pedidos`, {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(pedido)
